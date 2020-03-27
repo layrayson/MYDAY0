6060085715e376613499f7cba4f3aae8bc7cc1fb
Abdulazeez Olalere <olalereazeez11@gmail.com>
2020-03-27 15:32:55 +0100

    Initial commit

---
+++ main.js
@@ -0,0 +1,52 @@
+
+let bio = document.getElementById('bio');
+let gra = document.getElementById('gra');
+let wri = document.getElementById('wri');
+let men = document.getElementById('men');
+let spe = document.getElementById('spe');
+
+bio.addEventListener('mouseenter',function(){
+    bio.style.backgroundColor = 'white';
+    bio.style.webkitTextFillColor = 'tomato'
+})
+bio.addEventListener('mouseleave',function(){
+    bio.style.backgroundColor = 'tomato';
+    bio.style.webkitTextFillColor = 'rgb(16, 36, 117)'
+})
+gra.addEventListener('mouseenter',function(){
+    gra.style.backgroundColor = 'white';
+    gra.style.webkitTextFillColor = 'tomato'
+})
+gra.addEventListener('mouseleave',function(){
+    gra.style.backgroundColor = 'tomato';
+    gra.style.webkitTextFillColor = 'rgb(16, 36, 117)'
+})
+wri.addEventListener('mouseenter',function(){
+    wri.style.backgroundColor = 'white';
+    wri.style.webkitTextFillColor = 'tomato'
+})
+wri.addEventListener('mouseleave',function(){
+    wri.style.backgroundColor = 'tomato';
+    wri.style.webkitTextFillColor = 'rgb(16, 36, 117)'
+})
+men.addEventListener('mouseenter',function(){
+    men.style.backgroundColor = 'white';
+    men.style.webkitTextFillColor = 'tomato'
+})
+men.addEventListener('mouseleave',function(){
+    men.style.backgroundColor = 'tomato';
+    men.style.webkitTextFillColor = 'rgb(16, 36, 117)'
+})
+spe.addEventListener('mouseenter',function(){
+    spe.style.backgroundColor = 'white';
+    spe.style.webkitTextFillColor = 'tomato'
+})
+spe.addEventListener('mouseleave',function(){
+    spe.style.backgroundColor = 'tomato';
+    spe.style.webkitTextFillColor = 'rgb(16, 36, 117)'
+})
+   
+   
+   
+
+
